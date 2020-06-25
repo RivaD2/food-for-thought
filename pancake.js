e019894dfadb690ada8e26c865509f6b4562781c
RivaD2 <rivkadavidowski@gmail.com>
2020-06-24 19:38:18 -0700

    added js file to HTML and created js file

---
+++ pancake.js
@@ -0,0 +1,8 @@
+var greeting;
+var signup = confirm('Sign up for monthly recipes') 
+if (signup === true) {
+    greeting = 'Click here to sign up!'
+else if (signup === false) {
+    greeting = 'No pancakes for you!'
+}
+}
