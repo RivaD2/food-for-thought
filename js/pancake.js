1893a8bc035c6039d84f44dd21944693d3033663
RivaD2 <rivkadavidowski@gmail.com>
2020-06-24 19:57:39 -0700

    add closing curly brace to line 5

--- js/pancake.js
+++ js/pancake.js
@@ -2,7 +2,8 @@ var greeting;
 var signup = confirm('Sign up for monthly recipes') 
 if (signup === true) {
     greeting = 'Click here to sign up!'
-else if (signup === false) {
+}else if (signup === false) {
     greeting = 'No pancakes for you!'
 }
-}
+
+
